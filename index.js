d8480e9f4dd226150ff1c408d919fd4ade93daea
LingZhouChen <lzcciao@gmail.com>
2023-09-25 16:11:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titles = tutorials.map(title => {
+      title = title.split(" ");
+      
+      return title.map(word => {
+          return word[0].toUpperCase() + word.slice(1);
+      }).join(" ");
+
+  })
+  return titles;
 }
+
+console.log(titleCased())
+
+// const titleCased = () => {
+//   const titles = tutorials.map(title => {
+//     let temp = "";
+//     const wordArray = title.split(" ");
+
+//     for(word of wordArray) {
+//       word = word[0].toUpperCase() + word.slice(1);
+//       temp += " " + word;
+//     }
+//     return temp; 
+//   })
+//   return titles;
+// }
+
+// let test = tutorials[0].split(" ")
+// test = test[0].charAt(0).toUpperCase() + test[0].slice(1)
+
+
